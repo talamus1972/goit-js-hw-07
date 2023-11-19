070e3a72475b20276a0ea9a5b40372a46ef6a310
talamus1972 <talamus1972@gmail.com>
2023-11-19 12:23:40 +0200

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,11 @@
 
-const categor = document.querySelectorAll("ul");
+const categor = document.querySelector(".item");
+let el = 0;
+for (let i = 0; i < categor.length; i++) {
+  el = categor[i];
+  
+}
+
 // const categor = #catgories.forEach = (element => h2 === element);
-console.log(categor);
-https://web-dev.guru/javascript/foreach-js-metod-dlja-perebora-massiva/
+console.log(el);
+// https://web-dev.guru/javascript/foreach-js-metod-dlja-perebora-massiva/
