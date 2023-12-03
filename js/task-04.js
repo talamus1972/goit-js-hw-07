01901bb62ae87b4717c439efd54ad10fc06037e4
talamus1972 <talamus1972@gmail.com>
2023-12-03 17:01:50 +0200

    cccccc

--- js/task-04.js
+++ js/task-04.js
@@ -1,28 +1,54 @@
-const form = document.querySelector('.login-form');
-// const emailInput = document.querySelector('.email');
-// const passInput = document.querySelector('.password');
-let formData = {};
+// const form = document.querySelector('.login-form');
+// let formData = {};
 
+
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     const { elements: { email, password }, } = event.currentTarget;
+    
+//     if (email.value.trim() === '' || password.value.trim() === '') {
+        
+//         return alert('All form fields must be filled in');
+        
+//     } 
+
+    
+//     formData.email = email.value;
+//     formData.password = password.value;
+    
+//     console.log("👀 ~ formData:", formData);
+//     event.currentTarget.reset();
+
+//     const data = new FormData(event.target);
+//     data.forEach((value, key) => {
+//         console.log(value, key);
+//     });
+// });
+
+const form = document.querySelector('.login-form');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    const { elements: { email, password }, } = event.currentTarget;
-    
-    if (email.value.trim() === '' || password.value.trim() === '') {
-        
-        return alert('All form fields must be filled in');
-        
-    } 
 
     
-    formData.email = email.value;
-    formData.password = password.value;
-    
-    console.log("👀 ~ formData:", formData);
-    event.currentTarget.reset();
+    const emailValue = form.elements.email.value.trim();
+    const passwordValue = form.elements.password.value.trim();
 
-    // const data = new FormData(event.target);
-    // data.forEach((value, key) => {
-    //     console.log(value, key);
-    // });
+
+    if (emailValue === '' || passwordValue === '') {
+        alert('All form fields must be filled in');
+        return;
+    }
+
+
+    const formData = {
+        email: emailValue,
+        password: passwordValue,
+    };
+
+
+    console.log(formData);
+
+    
+    form.reset();
 });
--- js/task-05.js
+++ js/task-05.js
@@ -1,3 +1,20 @@
+// Напиши скрипт, який змінює колір фону елемента < body >
+//   через інлайн - стиль по кліку на button.change - color і 
+//   задає це значення кольору текстовим вмістом для span.color.
+
+// <div class="widget">
+//   <p>Background color: <span class="color">-</span></p>
+//   <button type="button" class="change-color">Change color</button>
+// </div>
+
+const btnChangeColor = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+
+btnChangeColor.addEventListener('click', () => {
+  !
+    
+})
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
