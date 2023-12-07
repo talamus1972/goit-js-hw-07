3d4d8c7b1e6d3ae522a6d609487bc399516eeb0d
talamus1972 <talamus1972@gmail.com>
2023-12-07 13:09:46 +0200

    2222222

--- js/task-02.js
+++ js/task-02.js
@@ -11,8 +11,19 @@ const images = [
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
+  {
+    url: 'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    alt: 'Elephant Beside on Baby Elephant',
+  },
+  {
+    url: 'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    alt: 'Blue Geeen and Orange Parrot',
+  },
+  {
+    url: 'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    alt: 'Zebras on Zebra',
+  },
 ];
-
 
 // const ulLiImg = document.querySelector('.gallery');
 // const fragment = document.createDocumentFragment();
@@ -32,9 +43,14 @@ const images = [
 // ulLiImg.appendChild(fragment);
 
 const ulLiImg = document.querySelector('.gallery');
-const colorImages = images.reduce((img, element) => img +`
-<li>
-<img src = '${element.url}' alt = '${element.alt}' />
+const colorImages = images.reduce(
+  (img, element) =>
+    img +
+    `
+<li style = " list-style: none">
+<img src = '${element.url}' alt = '${element.alt}' width = "300" height ="100%"/>
 </li>
-`, '')
+`,
+  ''
+);
 ulLiImg.insertAdjacentHTML('afterbegin', colorImages);
